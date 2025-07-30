afbc90c5cfc20e798a3eb2a00cdf850f24d3483f
dev-charan <raicharan100@gmail.com>
2025-07-30 18:02:37 +0530

    DB setup - Prisma

---
+++ propertylister/prisma/migrations/20250730122441_init/migration.sql
@@ -0,0 +1,66 @@
+-- CreateTable
+CREATE TABLE `Images` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `propertyId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Property` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `type` ENUM('RENT', 'SALE') NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `street` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `zipcode` VARCHAR(191) NOT NULL,
+    `bhk` ENUM('ONE_BHK', 'TWO_BHK', 'THREE_BHK', 'FOUR_BHK', 'ONE_RK') NOT NULL,
+    `area` INTEGER NOT NULL,
+    `parking` ENUM('YES', 'NO') NOT NULL,
+    `preferredTenant` ENUM('FAMILY', 'BACHELORS') NOT NULL,
+    `propertyType` ENUM('APARTMENT', 'INDEPENDENT') NOT NULL,
+    `availableFrom` DATETIME(3) NOT NULL,
+    `isFeatured` BOOLEAN NOT NULL DEFAULT false,
+    `isSold` BOOLEAN NOT NULL DEFAULT false,
+    `ownerId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_SavedProperties` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_SavedProperties_AB_unique`(`A`, `B`),
+    INDEX `_SavedProperties_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Images` ADD CONSTRAINT `Images_propertyId_fkey` FOREIGN KEY (`propertyId`) REFERENCES `Property`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Property` ADD CONSTRAINT `Property_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_SavedProperties` ADD CONSTRAINT `_SavedProperties_A_fkey` FOREIGN KEY (`A`) REFERENCES `Property`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_SavedProperties` ADD CONSTRAINT `_SavedProperties_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
